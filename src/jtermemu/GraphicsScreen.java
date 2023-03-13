0c49d6b354f0e7f74d19f2a64f191238aad247c1
Ekkehard Morgenstern <ekkehard@ekkehardmorgenstern.de>
2023-03-13 16:15:58 +0100

    added GPL info to source files; added Eclipse IDE files

--- src/jtermemu/GraphicsScreen.java
+++ src/jtermemu/GraphicsScreen.java
@@ -1,3 +1,26 @@
+/*  JTermEmu - a terminal emulator written in Java
+    Copyright (C) 2023  Ekkehard Morgenstern
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    NOTE: Programs created with PriamosBASIC do not fall under this license.
+
+    CONTACT INFO:
+        E-Mail: ekkehard@ekkehardmorgenstern.de
+        Mail: Ekkehard Morgenstern, Mozartstr. 1, D-76744 Woerth am Rhein, Germany, Europe 
+*/
+
 package jtermemu;
 
 import java.awt.Color;
